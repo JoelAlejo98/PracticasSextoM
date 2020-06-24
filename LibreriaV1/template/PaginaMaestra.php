361678563b1f58aa690923886a6d0caf97a1a2f7
user <user@USER-VAIO>
2020-06-24 00:27:22 -0500

    Se escribió reglas del negocio y mantenimiento.

--- LibreriaV1/template/PaginaMaestra.php
+++ LibreriaV1/template/PaginaMaestra.php
@@ -68,11 +68,11 @@ $nomUsu=$_SESSION['usuario'];
         Interface
       </div>
 
-      <!-- Nav Item - Pages Collapse Menu -->
+        <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+          <span>REGLES DEL  NEGOCIO</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
@@ -87,15 +87,15 @@ $nomUsu=$_SESSION['usuario'];
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
           <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
+          <span>MANTENIMIENTO</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <a class="collapse-item" href="editorial.php">Editorial</a>
+            <a class="collapse-item" href="libro.php">Libro</a>
+            <a class="collapse-item" href=autor.php>Autor</a>
+            <a class="collapse-item" href="ventas.php">ventas</a>
           </div>
         </div>
       </li>
